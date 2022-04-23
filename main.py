e760d28800c8624d42a15af773ad1752f213f15f
X - MrG3P5 <57594747+MrG3P5@users.noreply.github.com>
2022-04-23 20:12:58 +0700

    add
    add

---
+++ main.py
@@ -0,0 +1,42 @@
+import pyfiglet, os
+from colorama import Fore, init
+from multiprocessing.dummy import Pool as ThreadPool
+import requests as req
+
+red = Fore.LIGHTRED_EX
+cyan = Fore.LIGHTCYAN_EX
+white = Fore.WHITE
+green = Fore.LIGHTGREEN_EX
+
+try:
+    open("result.txt", "a")
+except:
+    pass
+
+init(autoreset=True)
+
+def __banner__():
+    os.system("cls||clear")
+    my_banner = pyfiglet.figlet_format("Cpanel-Checker", font="slant", justify="center")
+    print(f"{red}{my_banner}")
+    print(f"{cyan}\t\t\t[ {white}Created By X-MrG3P5 {cyan}]\n")
+
+def CpanelChecker(domain):
+    try:
+        req_data = req.get("http://"+ domain + ":2083", timeout=3)
+        if req_data.status_code == 200:
+            print(f"{cyan}[{green}200{cyan}] {white}{domain}:2083")
+            open("result.txt", "a").write(domain + ":2083" + "\n")
+        else:
+            print(f"{cyan}[{red}BAD{cyan}] {white}{domain}:2083")
+    except:
+        print(f"{cyan}[{red}BAD{cyan}] {white}{domain}:2083")
+
+if __name__=="__main__":
+    __banner__()
+    input_list = open(input(f"{cyan}[{white}?{cyan}] {white}Domain List : ")).read().replace("http://", "").replace("https://", "").splitlines()
+    Thread = input(f"{cyan}[{white}?{cyan}] {white}Thread : ")
+    pool = ThreadPool(int(Thread))
+    pool.map(CpanelChecker, input_list)
+    pool.close()
+    pool.join()
